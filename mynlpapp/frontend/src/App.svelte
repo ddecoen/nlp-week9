9aa44055cbcd7a0f79790f75b07c62e556b45eb7
ddecoen <ddecoen@gmail.com>
2023-08-20 07:33:55 -0700

    updates to code

--- mynlpapp/frontend/src/App.svelte
+++ mynlpapp/frontend/src/App.svelte
@@ -9,11 +9,19 @@
   let inputText = "";
   let outputText = "";
 
-  const validTerms = [
+  let validTermsVisible = false; // Initialize visibility state
+
+  const validTermList = [
     "break", "case", "chan", "const", "continue", "defer", "else", "fallthrough",
     "for", "func", "go", "goto", "if", "import", "interface", "map", "package",
     "range", "return", "select", "struct", "switch", "type", "var"
   ];
+
+  let terms = ["break", "case", "chan", "const", "continue", "defer", "else", "fallthrough",
+    "for", "func", "go", "goto", "if", "import", "interface", "map", "package",
+    "range", "return", "select", "struct", "switch", "type", "var"];
+
+  let validTerms = [];
 
   function evaluateText() {
     console.log("evaluateText function called with input:", inputText);
@@ -22,6 +30,20 @@
         outputText = result;
     });
 }
+
+  function handleButtonClick(){
+    if(validTermsVisible){
+      validTerms =[]; // clear the valid terms list
+    } else{
+      validTerms = terms.filter(term => ValidTermEmbed(term));
+    }
+    validTermsVisible = !validTermsVisible //toggle visibility state
+    
+  }
+
+  function ValidTermEmbed(term){
+    return validTermList.includes(term);
+  }
 
 </script>
 
@@ -46,19 +68,66 @@
   }
 
   .input-section {
-      display: flex;
-      align-items: start;
-      width: 90%;
+    display: flex;
+    align-items: start;
+    width: 90%;
   }
 
   textarea {
-      flex: 1;
-      height: 200px;
-      margin-right: 20px;
+    flex: 1;
+    height: 200px; /* Adjust the height as needed */
+    font-size: 20px; /* Increase the font size for a bigger text area */
   }
 
   button {
       margin-bottom: 20px;
+  }
+
+  .sample-buttons {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .styled-button {
+    padding: 10px 20px;
+    margin-bottom: 10px;
+    background-color: #007bff;
+    color: #ffffff;
+    border: none;
+    cursor: pointer;
+    border-radius: 5px;
+    font-size: 10px;
+  }
+
+  .valid-terms-list {
+    text-align: center;
+    margin-bottom: auto; /* Pushes the list to the bottom */
+    padding-bottom: 20px; /* Optional: Add some padding */
+  }
+
+  .horizontal-list {
+    display: flex;
+    justify-content: center;
+    padding: 0;
+  }
+
+  .vertical-column {
+    column-count: 3; /* Adjust the number of columns as needed */
+    column-gap: 20px; /* Adjust the gap between columns */
+    text-align: left; /* Align items to the left within columns */
+  }
+
+  .vertical-list li {
+    margin-bottom: 5px;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    margin-bottom: 5px;
   }
 
   .output {
@@ -77,6 +146,7 @@
 
 
 <div class="container">
+
   <div class="header-section">
       <!-- svelte-ignore a11y-img-redundant-alt -->
        <img src={helperImage} alt="Helper Image" class="helper-image" />
@@ -87,19 +157,33 @@
             </p>
         </div>
     </div>
+
     <div class="input-section">
-        <textarea bind:value={inputText} placeholder="Enter your text here..."></textarea>
-    </div>
+      <textarea bind:value={inputText} placeholder="Enter your text here..."></textarea>
+      <div class = "sample-buttons">
+        <button on:click={handleButtonClick} class ="styled-button">Show Valid Terms</button>
+      </div>
+  </div>
+
+    <div class="output">{outputText}</div>
+
     <div class="button-container">
         <button on:click={evaluateText}>Evaluate</button>
+   
+  {#if validTermsVisible}
+    <div class="valid-terms-list">
+      <h2>Valid Terms:</h2>
+      <div class="vertical-column">
+        <ul class="vertical-list">
+          {#each validTerms as term}
+            <li>{term}</li>
+          {/each}
+        </ul>
     </div>
-    <div class="output">{outputText}</div>
-    <div class="rules">
-        <h3>Valid Terms</h3>
-        <p>
-          "break", "case", "chan", "const", "continue", "defer", "else", "fallthrough",
-          "for", "func", "go", "goto", "if", "import", "interface", "map", "package",
-          "range", "return", "select", "struct", "switch", "type", "var"
-      </p>
+   </div>
+  {/if}
+  <div class="rules">
   </div>
-  </div>
+</div>
+</div>
+
